af2412691a2c131b09cfeda9e227c7bad4bf6af6
Damien Monasterios <monasterio13septiembre@gmail.com>
2021-07-21 21:17:08 -0500

    Repo para mis asignaciones

---
+++ Polidromo/script.js
@@ -0,0 +1,24 @@
+/*
+.split(''), lo separa en un array.
+.reverse() revierte el arreglo.
+.join('') Lo vuelve a convertir en string.
+*/
+  function polindromeChecker(str) {
+    const newStr = str.replace(/[\W_]/g, "").toLowerCase()//eliminamos los espacion en linea
+    const strReversed = newStr.split("").reverse().join("")
+  
+    if(strReversed === newStr){
+        resul = "es palindromo";
+     }else if(strReversed !== str){
+         resul = "no es palindromo";
+     }
+     return resul;
+  }
+
+let button = document.getElementById("btn");
+
+button.onclick = function(){
+   let pal = document.getElementById("str").value;
+   console.log(polindromeChecker(pal));
+   document.getElementById("result").innerHTML = polindromeChecker(pal);
+}
